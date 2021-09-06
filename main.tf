cf4233ef6f7b43a5ca614a834b82d8fb0fdb6c29
AlbinaGL <albina.aziz@globallogic.com>
2021-09-06 15:36:19 +0530

    remove envtest

--- main.tf
+++ main.tf
@@ -22,19 +22,35 @@ terraform {
 
 
 provider "aws" {
-  region = "us-east-1
+  region = "us-east-1"
 }
 
 
 
-
+resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-03295ec1641924349"
   instance_type          = "t2.micro"
-  
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 }
 
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-
-
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
